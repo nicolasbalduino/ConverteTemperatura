4a89c873d8f59d0fc2bcf64a0cc4fa0f41e7f731
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-04 18:48:12 -0300

    Add project files.

---
+++ ConverteTemperatura/Program.cs
@@ -0,0 +1,8 @@
+﻿double celsius, farenheit;
+
+Console.Write("Informe a temperatura em graus celsius: ");
+celsius = double.Parse(Console.ReadLine());
+
+farenheit = celsius * 1.8 + 32;
+
+Console.WriteLine("\nA temperatura e " + farenheit + "° Farenheit");
